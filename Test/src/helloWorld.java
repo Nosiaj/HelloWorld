7d308a5b0e6b9ca278993d07e96e0065e61b945f
Jaison <jaison@jaison-desktop>
2017-09-03 22:49:54 +0200

    Test

---
+++ Test/src/helloWorld.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class helloWorld {
+	
+	public static void hello(String name) {
+		System.out.println("Hello "+name);
+	
+	}
+	
+	
+		public static void main(String[] args) {
+		hello("World");
+		}
+}
